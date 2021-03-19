c9514773a46846890ca41375f062a43cdcc45b89
unknown <beham7biit@gmail.com>
2021-03-18 18:17:00 -1200

    Badhusha

--- TestJava/src/com/batch5/sgic/TestingHello.java
+++ TestJava/src/com/batch5/sgic/TestingHello.java
@@ -4,7 +4,7 @@ public class TestingHello {
 	public static void main(String args[]) {
 		System.out.println("welcome");
 		System.out.println("development branch");
-		System.out.println("It's Badhusha");
+		
 		
 	}
 }

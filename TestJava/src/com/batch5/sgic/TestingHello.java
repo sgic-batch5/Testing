208d5bee64b1da76495c62cd2b8f1699a9edded0
sinthuvamsan <sinthuvamsanv@gmail.com>
2021-03-19 13:03:42 +0530

    i am modify the file

--- TestJava/src/com/batch5/sgic/TestingHello.java
+++ TestJava/src/com/batch5/sgic/TestingHello.java
@@ -6,46 +6,40 @@ public class TestingHello {
 		System.out.println("development branch");
 		System.out.println("Risha Ahamadh");
 
-		System.out.println("Roshany") ;
-		
+		System.out.println("Roshany");
+
 		System.out.println("sasikala");
 
 		System.out.println("Prgs");
 
 		System.out.println("Prgs");
-		
+
 		System.out.println("Theepika");
 
 		System.out.println("Roshany");
 
 		System.out.println("I am from kilinochchi");
 
-		
 		System.out.println("Umasuthan");
 
 		System.out.println("Hi There I am Umasuthan");
-		
+
 		System.out.println("sasi");
-		
+
 		System.out.println("Umasuthan123");
-		
+
 		System.out.println("Hi anna");
 
 		System.out.println("Hi");
 
 		System.out.println("VS code");
-		
 
 		System.out.println("Thilakshana123");
 
 		System.out.println("hi everyone");
 
-<<<<<<< HEAD
 		System.out.println("hello w3 school");
-		
-=======
->>>>>>> e6c7a08dcebb63981637cf4cb5d7609a9d62ad37
 
-
+		System.out.println("This is aji");
 	}
 }

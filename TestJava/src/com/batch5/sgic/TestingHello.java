75af2c1a2d82b2f8db84907f7488ebebfca0ea68
Thilakshana123 <nayanthara39@gmail.com>
2021-03-19 11:47:16 +0530

    resolved

--- TestJava/src/com/batch5/sgic/TestingHello.java
+++ TestJava/src/com/batch5/sgic/TestingHello.java
@@ -4,6 +4,9 @@ public class TestingHello {
 	public static void main(String args[]) {
 		System.out.println("welcome");
 		System.out.println("development branch");
-		System.out.println("Roshany");
+		System.out.println("Roshany") ;
+		
+		
+		
 	}
 }

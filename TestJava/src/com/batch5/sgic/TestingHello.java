51424bb21d2cd797b235b288558d8cb51f9e0a90
unknown <gusuthan789@gmail.com>
2021-03-19 11:43:32 +0530

    my fitst commit

--- TestJava/src/com/batch5/sgic/TestingHello.java
+++ TestJava/src/com/batch5/sgic/TestingHello.java
@@ -4,16 +4,14 @@ public class TestingHello {
 	public static void main(String args[]) {
 		System.out.println("welcome");
 		System.out.println("development branch");
-<<<<<<< HEAD
-		System.out.println("Prgs");
-=======
 
+		System.out.println("Prgs");
+		
 		System.out.println("Theepika");
 
 		System.out.println("Roshany");
 		
-		System.out.println("Suthan");
+		System.out.println("Umasuthan");
 
->>>>>>> b6a65416ea5c691f1e12b21ead433272f8b88dba
 	}
 }
